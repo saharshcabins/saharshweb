dc095fe49af605bb447dc86fc5f3906906940fda
jitendravee <jitendravee098@gmail.com>
2025-10-26 11:21:46 +0530

    Fix Netlify build: disable ESLint/type checks during build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "public_html",
   eslint: {
     ignoreDuringBuilds: true, // ✅ disables ESLint errors during build
   },
