094752d19f3fbab4a20c17e0892a0fe413f513c0
saharshcabins <saharshcabins@users.noreply.github.com>
2026-05-31 22:41:08 +0530

    Test with resend.dev sender to isolate domain issue

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -2,4 +2,4 @@ import { Resend } from "resend";
 
 export const getResend = () => new Resend(process.env.RESEND_API_KEY);
 export const TO = "saharshcabins@gmail.com";
-export const FROM = "Saharsh Cabins <enquiries@saharsh.co>";
+export const FROM = "Saharsh Cabins <onboarding@resend.dev>";
