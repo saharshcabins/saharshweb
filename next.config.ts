790c02741df0c7f7c3297c2b495bbccf4192f778
saharshcabins <saharshcabins@users.noreply.github.com>
2026-06-11 20:29:20 +0530

    Enable Next.js image optimization and set quality to 75 for better performance

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,13 @@ const nextConfig: NextConfig = {
   },
   trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: [
       "media.istockphoto.com",
       "imgs.search.brave.com",
       "plus.unsplash.com",
       "images.unsplash.com",
     ],
+    quality: 75,
   },
 };
 
